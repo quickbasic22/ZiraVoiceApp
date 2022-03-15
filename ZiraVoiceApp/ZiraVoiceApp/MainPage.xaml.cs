d6527e26abd3496e4604014f19388302cc0d414a
David Morrow <quickbasic44@outlook.com>
2022-03-14 23:06:16 -0400

    Add project files.

---
+++ ZiraVoiceApp/ZiraVoiceApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace ZiraVoiceApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ZiraTalk_Clicked(object sender, EventArgs e)
+        {
+            var sO = new SpeechOptions();
+            sO.Pitch = 1.5f;
+            Xamarin.Essentials.TextToSpeech.SpeakAsync(TextTalk.Text, sO);
+           
+        }
+    }
+}
